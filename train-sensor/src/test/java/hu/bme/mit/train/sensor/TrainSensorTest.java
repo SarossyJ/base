a5f30221ab07f08fbfc429b6e8658eb142780eb7
xXCoffeeColaXc <64481275+xXCoffeeColaXc@users.noreply.github.com>
2022-05-16 17:16:06 +0200

    sensorTest added

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -7,13 +7,45 @@ import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
 
+    TrainSensor sensor; 
+    TrainController controller;
+    TrainUser user;  
+
     @Before
     public void before() {
         // TODO Add initializations
+        controller = mock(TrainController.class); 
+        user = mock(TrainUser.class); 
+        sensor = new TrainSensorImpl(controller, user);
+
     }
 
     @Test
-    public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+    public void alarmOverLimit() {
+        sensor.overrideSpeedLimit(501);
+
+        verify(user, times(1)).setAlarmState(true); 
+    }
+
+    @Test
+    public void alarmMinusSpeed() {
+        sensor.overrideSpeedLimit(-1);
+
+        verify(user, times(1)).setAlarmState(true); 
+    }
+
+    @Test
+    public void noAlarmAtNormalSpeed() {
+        sensor.overrideSpeedLimit(300);
+
+        verify(user, never()).setAlarmState(true); 
+    }
+
+    @Test
+    public void noAlarmAtNormalSpeedReturn() {
+        when(controller.getReferenceSpeed().thenReturn(202)); 
+        sensor.overrideSpeedLimit(100);
+
+        verify(user, times(1)).setAlarmState(true); 
     }
 }
