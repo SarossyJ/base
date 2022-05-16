66e4659e969669d69ad699dbeb1ece6e63c9b459
sarossyj <sarossyj@gmail.com>
2022-05-16 05:24:24 +0200

    traincontroller

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -8,6 +8,20 @@ public class TrainControllerImpl implements TrainController {
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
 
+		public TrainControllerImpl(){
+			thread = new Thread() {
+				public void run(){
+					thread.run();
+					thread.sleep(1000);
+				try {
+					followSpeed();
+				} catch (InterruptedException e) 	{
+					e.printStackTrace();
+				} 
+			} 
+		};
+	} 
+
 	@Override
 	public void followSpeed() {
 		if (referenceSpeed < 0) {
